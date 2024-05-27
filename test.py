74c0befc977cf680a078ac110477e5d242fe4e1f
JPMM-2 <84038900+JPMM-2@users.noreply.github.com>
2024-05-27 10:53:10 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 import sqlite as sql
 
-sql.insert (6573427258, 'fuan', '856995588', '2025-05-14')
+#sql.insert (6573427258, 'fuan', '856995588', '2025-05-14')
+
+sql.delete(6573427258)
+
+
 
 a = sql.exists(1256)
 if a:
